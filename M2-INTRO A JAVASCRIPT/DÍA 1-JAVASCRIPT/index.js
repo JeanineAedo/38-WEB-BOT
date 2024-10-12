5dff9cd41b4da79d4f94891d2ae5cc8cbc5ffbb8
Jeanine Aedo <rapshel.agmail.com>
2024-10-11 21:50:52 -0300

    Se agrega carpeta día 1 Java y variables

---
+++ M2-INTRO A JAVASCRIPT/DÍA 1-JAVASCRIPT/index.js
@@ -0,0 +1,15 @@
+var cualquierNombre1 = true;
+var cualquierNombre2 = 1;
+var cualquierNombre3 ="una cadena"
+var cualquierNombre4 = null
+var cualquierNombre5 = { llave: "valor"}
+var cualquierNombre6 = [{}, 1, "hola", [], undefined]
+var cualquierNombre7 = undefined
+
+
+console.log(cualquierNombre1)
+
+let userName = false;
+
+//No cambia//
+const variableConstante = "Chile"
