300474a1cb64b5f061839c4724155a6838c7634d
Jeanine Aedo <rapshel.agmail.com>
2024-10-15 17:52:28 -0300

    Se practica los operadores

---
+++ M2-INTRO A JAVASCRIPT/DÍA 2-OPERADORES/index.js
@@ -0,0 +1,27 @@
+console.log("hola mundo")
+
+let numero1 = 25;
+let numero2 = 23;
+
+let total = 0;
+
+total = numero1 + numero2
+
+total= numero1 - numero2
+
+total= numero1 * numero2
+
+total= numero1 / numero2
+
+total= numero1 % numero2
+
+numero1 ++
+
+total = numero1 < numero2
+
+
+if(numero1 > numero2){
+    console.log(numero1 + "es mayor que" + numero2);
+}else{
+    console.log(numero2 + "no es menor que" + numero1)
+}
