1c06be5461d992566c4d9b1fac77472f296961fc
Jeanine Aedo <rapshel.agmail.com>
2024-10-17 11:14:36 -0300

    Se ven los bucles for

---
+++ M2-INTRO A JAVASCRIPT/DÍA 3-BUCLES FOR/index.js
@@ -0,0 +1,29 @@
+
+//const numeroMaximo = 10
+//const arreglo = [2,4,6,8]
+//const frutas = ["Manzana", "uva", "pera"]
+//let indice = 0
+
+
+//for(indice; indice <= 3; indice = indice + 1){
+  //  console.log("Soy el indice" + indice);
+//}
+
+//arreglo.push(2)
+
+//const perfil = {
+  //  email: "fhbsdjkhfdjsk",
+    //nombre: "ajshjasha",
+   // edad: 22,
+//}
+
+//console.log(perfil.email)
+
+let numeros = [3,7,5]
+
+let total = 0
+for(let i = 0; i < numeros.length; i++) {
+    total = total + numeros(i);
+}
+
+console.log(total);
